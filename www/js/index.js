afe50e4bce5bb4a944728f32aa541a096e145f99
Jose <jose@practisis.com>
2015-12-08 12:13:46 -0500

    intento 1

---
+++ www/js/index.js
@@ -0,0 +1,47 @@
+    onDeviceReady();
+    function onDeviceReady(){
+		cordova.plugins.printer.isAvailable(
+			function (isAvailable) {
+				alert(isAvailable ? 'Service is available' : 'Service NOT available');
+			}
+		);
+    }
+
+   
+    var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        //aqui codigo de ondevide ready para empezar a hacer cosas.
+    },
+    escanear: function(){
+        resultDiv = document.querySelector("#results");
+    resultDiv.innerHTML='';
+        cordova.plugins.barcodeScanner.scan(
+        function (result) {
+                        alert(result.text);
+        }, 
+        function (error) {
+            alert("Error de Scan: " + error);
+        }
+    );
+        
+    }
+};
